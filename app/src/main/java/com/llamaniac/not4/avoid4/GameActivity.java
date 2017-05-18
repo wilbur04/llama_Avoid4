9695350e6302df3514b555850aa676feec5e996a
Tom Oborne <tpoborne@gmail.com>
2017-05-18 11:06:38 +0100

    Fixed not round buttons problem.

--- app/src/main/java/com/llamaniac/not4/avoid4/GameActivity.java
+++ app/src/main/java/com/llamaniac/not4/avoid4/GameActivity.java
@@ -2,6 +2,7 @@ package com.llamaniac.not4.avoid4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +24,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
 
         color_green = getResources().getColor(R.color.player_1);
         color_blue = getResources().getColor(R.color.player_2);
